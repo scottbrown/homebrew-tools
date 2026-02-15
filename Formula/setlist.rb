86ee789e5ea43054464c100d936bf3f0e52014ca
Scott Brown <scottbrown@users.noreply.github.com>
2026-02-15 13:24:59 -0700

    Upgrading setlist to v2.0.0

--- Formula/setlist.rb
+++ Formula/setlist.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-VERSION = '1.4.0'
+VERSION = '2.0.0'
 PACKAGE = 'setlist'
 REPO = "https://github.com/scottbrown/#{PACKAGE}"
 HASHES = {
-  darwin_amd64: '7888a679c638d38ebcc1dd4c42ceea08e7485ba06bf85b1a808b7272c943f5aa',
-  darwin_arm64: 'a5985a34ff7ee563b9b3217393bbecc716151b3c96abcf132cade3891039cfa0',
-  linux_amd64: '7fd51ac317c29ebad99559a04295738a2c0cdb944bfc4a12dcc477289e23032f',
-  linux_arm64: '4e95d4b5be12643c0bc2d66367394f9178ce67fdae42b690bf5ddad24cd2f676',
+  darwin_amd64: '58337beaf74f898b8f5fdeb57ea312fe7c5801e71d895525dde0a8dedccb9ead',
+  darwin_arm64: 'd29b611880dc48183ecc6505f0f443be190a67b48af24aac58aa911ec2d05d47',
+  linux_amd64: '45c80d9bf76fbf8827fad9bed46a33981f033a7df2abe306f5d6df723bfb1e57',
+  linux_arm64: '8f0528c7e104201697435991c6d2d8c6972494ba36f47f63ae29129594e4a2fc',
 }
 
 # Homebrew formula
